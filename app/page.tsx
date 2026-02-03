92e576d5e260325dc679f2f45cb2af940ec0b7ee
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-03 14:30:53 +0200

    ...

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600">
+        Will you be my Valentine? ❤️
+      </h1>
+    </main>
+  )
+}
