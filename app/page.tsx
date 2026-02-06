eb77d5b191a3cb8b819dc74cf7ac249653c09843
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-06 11:03:58 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 'use client'
-
-import { motion } from 'framer-motion'
 import ValentineCard from '@/components/ValentineCard'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-pink-200 via-rose-100 to-red-200">
