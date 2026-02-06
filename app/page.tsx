6cda2aa84f816d6e369955960c2f6570b71edbc3
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-06 11:13:15 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-'use client'
 import ValentineCard from '@/components/ValentineCard'
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-pink-200 via-rose-100 to-red-200">
+    <main className="flex min-h-screen items-center justify-center px-6">
       <ValentineCard />
     </main>
   )
