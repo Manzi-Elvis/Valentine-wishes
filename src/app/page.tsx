7561849a47243f951fd739418e35cc9fff481a87
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-06 11:27:54 +0200

    ...

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="rounded-3xl bg-gradient-to-r from-pink-500 to-red-500 p-12 text-4xl font-extrabold text-white shadow-2xl">
+        Tailwind v4 is working 💖
+      </div>
+    </div>
+  )
+}
