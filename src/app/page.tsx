4e515e4a5a383c8a8d481c6203b6450e0697bcc5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-08 13:37:33 +0200

    ...

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import ValentineCard from '@/components/ValentineCard'
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center px-6">
-      <ValentineCard />
-    </main>
-  )
-}
