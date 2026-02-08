0f7164bc5e5928068e9fb2de2e9a648882c816ee
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-08 13:43:38 +0200

    ...

--- app/valentine/[id]/page.tsx
+++ app/valentine/[id]/page.tsx
@@ -3,3 +3,10 @@
 import { useEffect, useState } from 'react'
 import ValentineExperience from '@/components/ValentineExperience'
 import { useParams } from 'next/navigation'
+
+interface ValentineData {
+  senderName: string
+  partnerName: string
+  message: string
+  songPlaylist: string
+}
