f1b3654752545837da0a6a8bc6695181ff0cf71b
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-09 22:00:31 +0200

    ...

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import ValentineSetup from '@/components/ValentineSetup'
-import FloatingHearts from '@/components/FloatingHearts'
-import ValentineCard from '@/components/ValentineCard'
