414e93b00448de203649c3895ff4eb8e0d827933
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-02-11 21:48:59 +0200

    ...

--- components/Confetti.tsx
+++ components/Confetti.tsx
@@ -14,7 +14,7 @@ export default function Confetti() {
   const [confetti, setConfetti] = useState<ConfettiPiece[]>([])
 
   useEffect(() => {
-    const emojis = ['❤️', '💖', '✨', '🥰', '💝', '🌹', '💕', '⭐']
+    const emojis = ['❤️', '💖', '✨', '🥰', '💝', '🌹', '💕','💞', '⭐']
 
     const pieces: ConfettiPiece[] = Array.from({ length: 50 }).map((_, i) => ({
       id: i,
